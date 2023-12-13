3c62f33205a6940dbd472a467d1fa27a1a2fe2de
CİHAN DÜRMÜŞ <44983119+chndrms@users.noreply.github.com>
2023-12-13 08:02:53 +0000

    başlangıç

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_Core_CI_CD.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "TestCase")]
+    public string Get()
+    {
+       return "Denemeler";
+    }
+}
